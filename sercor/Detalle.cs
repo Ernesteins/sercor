0f08623e2264c676fa05268e30a73909468ce0a2
Ernesteins <eyaselga@hotmail.com>
2018-07-29 12:28:01 -0500

    sercor db versión 1.3 e inicio de detalle

--- sercor/Detalle.cs
+++ sercor/Detalle.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace sercor
 {
-    public partial class Detalle : Form
+    class Detalle
     {
-        public Detalle()
-        {
-            InitializeComponent();
-        }
+        public int ID_DETALLE { get; set; }
+        public int ID_FACTURA { get; set; }
+        public int ID_USUARIO { get; set; }
+        public decimal IVA { get; set; }
+        public decimal TOTAL { get; set; }
+        public string FECHA { get; set; }
+        public decimal FACTOR_DESCUENTO { get; set; }
+        public decimal VALOR_DESCONTADO { get; set; }
+        public int TIPO { get; set; }
+        public int INDICE { get; set; }
     }
 }
