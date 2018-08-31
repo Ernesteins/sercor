4dbe4a6e37ca4cd746f0ed05c42caed445746106
Ernesto Yaselga <eyaselga@hotmail.com>
2018-08-31 03:18:56 -0500

    grde

--- sercor/sercormain.Designer.cs
+++ sercor/sercormain.Designer.cs
@@ -545,7 +545,7 @@
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 103F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 104F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(1232, 104);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
